4c3406294adc2f35be91ec2a53e758a54797907a
Lucas <lucasvalenteds@gmail.com>
2022-11-08 21:03:45 -0300

    Implementing VehicleDetails

--- src/main/resources/db/migration/V1__create_vehicle_table.sql
+++ src/main/resources/db/migration/V1__create_vehicle_table.sql
@@ -2,6 +2,7 @@ CREATE TABLE vehicle
 (
     vehicle_id          BIGINT AUTO_INCREMENT,
     vehicle_description VARCHAR(45),
+    vehicle_details     JSON,
 
     CONSTRAINT pk_vehicle_id PRIMARY KEY (vehicle_id)
 );
